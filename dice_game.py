bec4fdb4dcfd73dce5b61a23e5c840806aa6fdbf
Stephen Dice <jetayor@gmail.com>
2023-10-03 12:53:26 +0000

    OOP

--- dice_game.py
+++ dice_game.py
@@ -10,3 +10,31 @@ class Dice:
 
     # Methods
     def roll():
+        return
+
+
+class Player:
+    # Attributes
+    name = ""
+
+    # Constructor
+    def __init__(self, name):
+        self.name = name
+
+    # Methods
+    def play():
+        return
+
+
+class Board:
+    # Atributes
+    color = ""
+
+
+    # Constructor
+    def __init__(self, color):
+        self.color = color
+
+    # Methods 
+    def switch():
+        return  
